108d48addb5a74f241ea4560a8ea48b292d74ac5
Taha-Sid <74993189+Taha-Sid@users.noreply.github.com>
2021-01-25 12:20:24 +0000

    Add files via upload
    The pieces are now appearing just glitched at the top left corner

--- draughts.py
+++ draughts.py
@@ -17,10 +17,51 @@ done = False
  
 clock = pygame.time.Clock()
 
-box_y = 5
-box_x = 5
+square_size = 5
 
-board = [[1,0,1,0,1,0,1,0],
+yellow_circle = pygame.image.load("yellow_circle.png")        
+
+class Blacksquare(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((75,75))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+class Brownsquare(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((75,75))
+        self.image.fill(BROWN)
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+class Piece(pygame.sprite.Sprite):
+    def __init__(self,row,column):
+        self.row = row
+        self.column = column
+        self.x = 0
+        self.y = 0
+        self.calculate_position()
+
+    def calculate_position(self):
+        self.x = square_size * self.column + square_size // 2
+        self.y = square_size * self.row + square_size // 2
+        
+    def draw_white(self):
+        radius = square_size // 2
+        pygame.draw.circle(screen,WHITE,(self.x,self.y),radius)
+
+class Board(pygame.sprite.Sprite):
+    def __init__(self):
+        self.selected_piece = None
+        self.green_pieces_left = self_white_pieces_left = 12
+
+    def draw_squares(self):
+        board = [[1,0,1,0,1,0,1,0],
          [0,1,0,1,0,1,0,1],
          [1,0,1,0,1,0,1,0],
          [0,1,0,1,0,1,0,1],
@@ -29,51 +70,34 @@ board = [[1,0,1,0,1,0,1,0],
          [1,0,1,0,1,0,1,0],
          [0,1,0,1,0,1,0,1]]
         
+        for y in range(8):
+            for x in range(8):
+                if board[y][x] == 1:
+                    brownsquare = Brownsquare(x*75,y*75)
+                    all_sprites_list.add(brownsquare)
+                    brownsquare_list.add(brownsquare)
+                    pygame.draw.circle(screen,WHITE,(x,y),square_size)
+                    #piece = Piece(x,y)
+                    #piece.draw_white()
+                else:
+                    blacksquare = Blacksquare(x*75,y*75)
+                    all_sprites_list.add(blacksquare)
+                    blacksquare_list.add(blacksquare)
 
-class BlackBox(pygame.sprite.Sprite):
-    def __init__(self,x,y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('yellow_circle.png')
-        self.rect = self.image.get_rect()
-        #self.image = pygame.Surface((75,75))
-        #self.image.fill(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-
-class BrownBox(pygame.sprite.Sprite):
-    def __init__(self,x,y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('blue_circle.png')
-        self.rect = self.image.get_rect()
-        #self.image = pygame.Surface((75,75))
-        #self.image.fill(BROWN)
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-
-
-brownbox_list = pygame.sprite.Group()
-blackbox_list = pygame.sprite.Group()
+board = Board()
+brownsquare_list = pygame.sprite.Group()
+blacksquare_list = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
-
-
-for y in range(8):
-    for x in range(8):
-        if board[y][x] == 1:
-            brownbox = BrownBox(x*75,y*75)
-            all_sprites_list.add(brownbox)
-            brownbox_list.add(brownbox)
-        else:
-            blackbox = BlackBox(x*75,y*75)
-            all_sprites_list.add(blackbox)
-            blackbox_list.add(blackbox)
-            
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pass
+
+    board.draw_squares()
  
     pygame.display.update()
     screen.fill(WHITE)
